23e2a689c2797daa09513ea679961b83a187942e
Minhaj06 <milonmdminhajkobir@gmail.com>
2023-04-02 23:06:37 +0600

    Changed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express=require('express');
-const helmet=require("helmet")
-const mongoose=require("mongoose")
+const express = require("express");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
 
-
-const app=express();
+const app = express();
 app.use(helmet());
 
+// I am MINHAJ
 //add mongoose
 //ad router vvv
